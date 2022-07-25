592e0911b2657e84e15099db7c3a3b7feefd82ef
Eve Morgan <evemorgan27@gmail.com>
2022-07-25 12:31:36 -0700

    tvmaze

--- tvmaze.ts
+++ tvmaze.ts
@@ -1,10 +1,29 @@
 import axios from "axios";
-import * as $ from 'jquery';
+import * as $ from "jquery";
 
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+const BASE_URL = "https://api.tvmaze.com";
+const DEFAULT_IMG = "https://tinyurl.com/tv-missing";
+
+interface ShowInterface {
+  id: number;
+  name: string;
+  summary: string;
+  image: string;
+}
+
+interface ShowAPIInterface {
+  score: number;
+  show: {
+    id: number;
+    name: string;
+    summary: string;
+    image: { medium: string; } | null;
+  };
+}
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,28 +32,22 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
+async function getShowsByTerm(term: string) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  console.log("inside")
+  const resp = await axios.get(
+    `${BASE_URL}/search/shows?q=${term}`
+  );
+  console.log(resp);
+  const shows: ShowInterface[] = resp.data.map((s: ShowAPIInterface) => ({
+    id: s.show.id,
+    name: s.show.name,
+    summary: s.show.summary,
+    image: (s.show.image?.medium || DEFAULT_IMG)!,
+  }));
+  console.log(shows);
+  return shows;
 }
-
 
 /** Given list of shows, create markup for each and to DOM */
 
@@ -43,11 +56,11 @@ function populateShows(shows) {
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src=${show.image}
+              alt=${show.name}
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -58,11 +71,12 @@ function populateShows(shows) {
            </div>
          </div>
        </div>
-      `);
+      `
+    );
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
-
 
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
@@ -80,7 +94,6 @@ $searchForm.on("submit", async function (evt) {
   evt.preventDefault();
   await searchForShowAndDisplay();
 });
-
 
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
